5066244bf7c9d5a790744f14357b6934c0defb0b
mariaeduarda-cat <93399922+mariaeduarda-cat@users.noreply.github.com>
2022-03-05 21:18:02 -0300

    exercicios

--- teste/teste-c/teste.c
+++ teste/teste-c/teste.c
@@ -1,8 +1,9 @@
 #include <locale.h>
 #include <stdio.h>
 
-void main () {
+int main () {
     setlocale(LC_ALL, "Portuguese");
+    system("cls");
 
     /*int a, b, c;
     a = 10;
@@ -30,12 +31,95 @@ void main () {
    printf("O sucessor de b é a letra: %c.\n", escolha);*/
 
 
-    char inicial;
-    int idade;
-    printf(?Entre com a sua idade e a sua inicial:\n?);
-    scanf(?%d?, &idade);
-    scanf(?%c?, &inicial);
-    printf(?Voce tem %d anos e seu nome comeca com %c\n?, idade, inicial);
+    /*int cadastrado, ativo, logado;
+    char opcao;
+
+    cadastrado = ativo = logado = 0;
     
+    printf("Deseja cadastrar sua conta? S/N \n");
+    scanf(" %c", &opcao);
+
+    if(opcao == 'S') {
+        cadastrado = 1;
+        printf("\nConta cadastrada. \n");
+    }
+
+    printf("Deseja ativar sua conta? S/N \n");
+    scanf(" %c", &opcao);
+
+    if(opcao == 'S') {
+        ativo = 1;
+        printf("\nConta ativada. \n");
+    }
+
+    printf("Deseja logar sua conta? S/N \n");
+    scanf(" %c", &opcao);
+
+    if(opcao == 'S') {
+        logado = 1;
+        printf("Conta logada. \n");
+    }
+
+    if((cadastrado == 1) && (ativo == 1) || (logado == 1)) {
+        printf("\nSeja bem vindo.\n");
+    } 
+    else {
+        printf("\nAlgo deu errado.\n");
+    }*/
+
+    /*float nota1, nota2, nota3, media, total;
+
+    printf("Informe sua primeira nota:\n");
+    scanf("%f", &nota1);
+    fflush(stdin);
+
+    printf("Informe sua segunda nota:\n");
+    scanf("%f", &nota2);
+    fflush(stdin);
+
+    printf("Informe sua terceira nota:\n");
+    scanf("%f", &nota3);
+
+    media = (nota1 + nota2 + nota3) / 3.0;
+    total = media;
+
+    if(media >= 8) {
+        printf("\nSua nota final foi de %.2f. \nParábens!!! Você foi aprovado.", total);
+    }
+    else {
+        printf("SUa nota final foi de %.2f. \nInfelizmente você foi reprovado!", total);
+    }*/
+
+    int opcao;
+
+    printf("Entre 1° para adicionar ou 2° para excluir: \n");
+    scanf("%d", &opcao);
+
+    /*if(opcao >= 0) {
+        if (opcao == 0) {
+            printf("\nNúmero nulo.\n");
+        } else {
+            printf("\nNúmero positivo.\n");
+        }
+    } else {
+        printf("\nNúmero negativo.\n");
+    }*/
+
+    switch(opcao) {
+        case 1: {
+            printf("\nNúmero 1.\n");
+            break;
+        }
+    
+        case 2: {
+            printf("\nNúmero 2.\n");
+            break;
+        }
+
+        default: printf("\nOpção inválida.\n");
+    }
+
+    printf("Final do programa");
+
 
 }
